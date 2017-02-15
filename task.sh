c24e436bf5e006fb00ae2d38749d3f3c12790196
Matúš Bako <bako.matus@gmail.com>
2017-02-15 16:07:44 +0100

    script for metacentrum task

---
+++ task.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#PBS -N NN_training
+#PBS -q gpu
+#PBS -l scratch=300mb
+#PBS -l walltime=2h
+#PBS -l nodes=1:ppn=16:gpu=2
+#PBS -l mem=1gb
+
+## launch as: qsub task.sh
+
+module add python-2.7.10-gcc
+module add cuda-7.5
+module add cudnn-5.0
+module add caffe2016-gpu-cudnn
+
+home="/storage/brno2/home/hacky"
+
+trap 'clean_scratch' TERM EXIT
+trap 'cp -r $SCRATCHDIR/log.out $SCRATCHDIR/*npz $SCRATCHDIR/trained $home && clean_scratch' TERM
+
+#copy data
+cp -r $home/arrays.py $home/boards2.npz $home/train2.py $home/trainQ.sh $home/queue $SCRATCHDIR
+mkdir $SCRATCHDIR/trained
+
+#launch task
+cd $SCRATCHDIR
+
+./trainQ.sh ./
+
+#copy results to home
+cp -r ./trained $home
+
+#clean scratch
+clean_scratch
