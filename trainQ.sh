5ed65d0a6198d4feacb85b22c55570ec6a08d981
Matúš Bako <bako.matus@gmail.com>
2017-02-17 03:47:31 +0100

    draw_net disabled - pyplot unsupported on metacentrum

--- trainQ.sh
+++ trainQ.sh
@@ -27,14 +27,12 @@ fi
 mkdir -p ${top_dir}/trained
 
 #iterate over all directories containing *.solverstate files (net and solver)
-for net_dir in `ls ${top_dir}/queue` # 
+for net_dir in `ls ${top_dir}/queue`
 do
 	#get paths
 	solver=`ls ${top_dir}/queue/${net_dir} | grep ".*solver.prototxt$"`
-	#solver=`realpath ${top_dir}/queue/${net_dir}/$solver`
 
 	net=`ls ${top_dir}/queue/${net_dir} | grep ".*val.prototxt$"`
-	#net=`realpath ${top_dir}/queue/${net_dir}/$net`
 
 	#copy *.solverstate files to location of training script
 	cp ${top_dir}/queue/${net_dir}/${solver} ${top_dir}
@@ -44,33 +42,36 @@ do
     cat net_solver.prototxt | sed 's/CPU/GPU/g' > net_solver.prototxt
 
 	#train net
-	${top_dir}/train2.py -i=10000 -s=${top_dir}/queue/${net_dir}/${solver} -v
+	${top_dir}/train2.py -i=20000 -s=${top_dir}/queue/${net_dir}/${solver} -v
 
 	if [ $? -ne "0" ]; then
-		echo "Error occured while training!" && exit
+		echo "Error occured during training!" && exit
 	else
 		#name of directory where to copy results
 		res_dir=${top_dir}/trained/${net_dir}
 
-		#draw net
-		${top_dir}/draw_net.py ${top_dir}/queue/${net_dir}/net_train_val.prototxt net.svg
+        # create or clean directory containing results of computations
+        if [ -d ${res_dir} ]; then
+            rm -r ${res_dir}/*
+        else
+		    mkdir "${res_dir}"
+        fi
 
-		mkdir "${res_dir}"
-		cp ${top_dir}/probs*.svg ${res_dir}
-		cp ${top_dir}/loss*.svg ${res_dir}
-		cp ${top_dir}/stats*.npz ${res_dir}
-		cp ${top_dir}/*.solverstate ${res_dir}
-		cp ${top_dir}/*.caffemodel ${res_dir}
-		cp ${top_dir}/*.prototxt ${res_dir}
-		cp ${top_dir}/net.svg ${res_dir}
+		mv ${top_dir}/stats*.npz ${res_dir}
+		mv ${top_dir}/*.solverstate ${res_dir}
+		mv ${top_dir}/*.caffemodel ${res_dir}
+		mv ${top_dir}/*.prototxt ${res_dir}
 
-        rm ${top_dir}/probs*.svg
-        rm ${top_dir}/loss*.svg
-        rm ${top_dir}/stats*.npz
-        rm ${top_dir}/*.solverstate
-        rm ${top_dir}/*.caffemodel
-        rm ${top_dir}/*.prototxt
-		rm ${top_dir}/net.svg
+        # this directory exists on Metacentrum and
+        # usually not on your PC
+		if [ ! -d /storage ]; then
+            #draw net
+            ${top_dir}/draw_net.py ${top_dir}/queue/${net_dir}/net_train_val.prototxt net.svg
+
+            mv ${top_dir}/probs*.svg ${res_dir}
+            mv ${top_dir}/loss*.svg ${res_dir}
+            mv ${top_dir}/net.svg ${res_dir}
+        fi
 	fi
 
 
